03d1a1903a66299cb0169295745f74ee51ca3a81
biad-iritie <biadboze@gmail.com>
2022-03-28 18:58:39 +0800

    Done loading page

---
+++ lib/pages/loading.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:world_time/services/world_time.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime data = WorldTime(
+        location: 'Beijin / 北京', flag: 'germany.png', url: 'Asia/Hong_Kong');
+    await data.getTime();
+    /* Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+    }); */
+    Navigator.pushReplacementNamed(context, '/home', arguments: data);
+    //print(instance.time);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitRotatingCircle(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/world_time.dart
@@ -0,0 +1,34 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+  final String location; //location name for the UI
+  late String time; // time in that location
+  final String flag; // url to an asset icon
+  final String url; // location url for api endpoint
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+    try {
+      // Make the request
+      final Uri req_url = Uri.http('worldtimeapi.org', '/api/timezone/$url');
+      http.Response res = await http.get(req_url);
+      Map data = jsonDecode(res.body);
+      //http.Response response = await http.get(url);
+      //print(response);
+
+      // get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      // Set the time property
+      time = DateFormat.jm().format(now);
+    } catch (e) {
+      print('Caught error: $e');
+      time = 'Could not get time data';
+    }
+  }
+}
